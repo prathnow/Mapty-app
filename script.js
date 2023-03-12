67c5decb4a53104d449686c441fadd382ae91794
prathnow <prathnow@gmail.com>
2023-03-12 11:27:22 +0100

    Add calcSpeed() method to class Cycling

--- script.js
+++ script.js
@@ -41,6 +41,12 @@ class Cycling extends Workout {
     super(coords, distance, duration);
     this.elevationGain = this.elevationGain;
   }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
 }
 
 class App {
