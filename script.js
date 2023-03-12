c9cb794c52c4fc73e6ab48e2cb39006ff1b895b4
prathnow <prathnow@gmail.com>
2023-03-12 11:21:26 +0100

    Add calcPace() method to class Running

--- script.js
+++ script.js
@@ -26,6 +26,12 @@ class Running extends Workout {
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
   }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
 }
 
 class App {
