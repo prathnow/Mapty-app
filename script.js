aaff99c76c1b374222df052c29fadd97bd892ca5
prathnow <prathnow@gmail.com>
2023-03-15 21:51:36 +0100

    Add _hideForm() method

--- script.js
+++ script.js
@@ -110,6 +110,19 @@ class App {
     inputDistance.focus();
   }
 
+  _hideForm() {
+    // Empty inputs
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    form.style.display = 'none';
+    form.classList.add('hidden');
+    setTimeout(() => (form.style.display = 'grid'), 1000)
+  }
+
   _toggleElevationField() {
     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
@@ -169,11 +182,7 @@ class App {
     this._renderWorkout(workout);
 
     // Hide form and clear input fiels
-    inputDistance.value =
-      inputDuration.value =
-      inputCadence.value =
-      inputElevation.value =
-        '';
+    this._hideForm();
   }
 
   _renderWorkoutMarker(workout) {
